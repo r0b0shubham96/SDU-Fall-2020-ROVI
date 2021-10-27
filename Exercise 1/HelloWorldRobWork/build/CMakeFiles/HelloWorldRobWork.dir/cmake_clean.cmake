02babcb9a29e8dff6eb9f29053833236b5849c26
Shubham Kumar <kkshubham2396@gmail.com>
2021-10-27 08:31:00 +0200

    Robotics

---
+++ Exercise 1/HelloWorldRobWork/build/CMakeFiles/HelloWorldRobWork.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldRobWork.dir/src/HelloWorldRobWork.cpp.o"
+  "HelloWorldRobWork.pdb"
+  "HelloWorldRobWork"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorldRobWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
